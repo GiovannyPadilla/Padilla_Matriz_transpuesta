afb45dbee61b5543b7284e2cb770836f6f6369c5
giovanny.padilla@udla.edu.ec <giovanny.padilla@udla.edu.ec>
2023-11-27 16:22:17 -0500

    Creacion de variables

---
+++ matriz.c
@@ -0,0 +1,24 @@
+//Uso de las librerias
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+int main(int argc, char const *argv[])
+{
+    //creacion de variables
+    int matriz, filas, columna;
+    //pedir al usuario que ingrese numero de filas y  de columnas en la matriz
+    printf("Ingrese el numero de filas\n");
+    scanf("%d", &filas);
+    printf("Ingrese el numero de columnas\n");
+    scanf("%d", &columna);
+    for (int n = 0; n < 100; n++)
+    {
+        for (int m = 0; m < 100; m++)
+        {
+            /* code */
+        }
+        
+    }
+    
+    return 0;
+}
