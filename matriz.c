f0b6074c4ae417011dd5310023b955d1282087a9
giovanny.padilla@udla.edu.ec <giovanny.padilla@udla.edu.ec>
2023-11-27 16:41:41 -0500

    uso del srand para generar numeros aleatorios

--- matriz.c
+++ matriz.c
@@ -1,24 +1,46 @@
-//Uso de las librerias
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <time.h>
+
 int main(int argc, char const *argv[])
 {
-    //creacion de variables
-    int matriz, filas, columna;
-    //pedir al usuario que ingrese numero de filas y  de columnas en la matriz
-    printf("Ingrese el numero de filas\n");
-    scanf("%d", &filas);
-    printf("Ingrese el numero de columnas\n");
-    scanf("%d", &columna);
-    for (int n = 0; n < 100; n++)
-    {
-        for (int m = 0; m < 100; m++)
-        {
-            /* code */
-        }
-        
-    }
+    // Creación de variables
+    int filas, columnas;
     
+    // Pedir al usuario que ingrese el número de filas y columnas en la matriz
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &filas);
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &columnas);
+
+    int matriz[filas][columnas];
+    int transpuesta[columnas][filas];
+
+    srand(time(NULL));
+
+    printf("\nMatriz original:\n");
+
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            matriz[i][j] = rand() % 100; // Generar un número aleatorio entre 0 y 99
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nMatriz transpuesta:\n");
+
+    for (int i = 0; i < columnas; i++)
+    {
+        for (int j = 0; j < filas; j++)
+        {
+            transpuesta[i][j] = matriz[j][i];
+            printf("%d ", transpuesta[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
